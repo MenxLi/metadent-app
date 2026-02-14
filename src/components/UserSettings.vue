017b0b54e87d8cbc3e722835a7de5cfe4e2cb483
WenhDeng <2020312011047@whu.edu.cn>
2026-02-14 15:22:14 +0800

    Update AI backend URL placeholder text

--- src/components/UserSettings.vue
+++ src/components/UserSettings.vue
@@ -89,7 +89,7 @@ function saveSettings() {
         <input
           v-model="aiBackendUrl"
           type="text"
-          placeholder="http://xxx.xx.xx.xx:xxxx/infer"
+          placeholder="https://xxx.com:xxxxx"
           class="mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm
                  focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -86,7 +86,7 @@ async function handleLogin() {
           <input
             type="text"
             v-model="settings.aiBackendUrl"
-            placeholder="http://xxx.xx.xx.xx:xxxx/infer"
+            placeholder="https://xxx.com:xxxxx"
             class="w-full px-4 py-2 border rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
           />
         </div>
